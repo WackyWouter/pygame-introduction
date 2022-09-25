c10dd88ceb4231287f25bbe22d5bbebb5a8f4258
Wouter Bosch <wfcbosch@gmail.com>
2022-09-25 21:16:19 +0100

    finished small runner game

--- game.py
+++ game.py
@@ -1,74 +1,114 @@
 import pygame
+from random import choice
+from player import Player
+from obstacle import Obstacle
+
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = font.render(f'Score: {current_time}', False, (64,64,64))
+    score_rect = score_surf.get_rect(center = (400, 50))
+    screen.blit(score_surf, score_rect)
+    
+    return current_time
+
+def collision_sprite():
+    if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
+        obstacle_group.empty()
+        return False
+    else: return True
+
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 font = pygame.font.Font('font/Pixeltype.ttf', 50)
-game_active = True
+game_active = False
+start_time = 0
+score = 0
+bg_music = pygame.mixer.Sound('audio/music.wav')
+bg_music.set_volume(0.05)
+# Play the audio forever
+bg_music.play(loops = -1)
+
+# groups
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+obstacle_group = pygame.sprite.Group()
 
 sky = pygame.image.load('graphics/Sky.png').convert()
 ground = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = font.render('My game', False, (64, 64, 64))
-score_rect = score_surf.get_rect(center = (400, 50))
+# Intro screen
+player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center = (400, 200))
 
-# Use convert_alpha to remove the white background
-snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(bottomright = (600, 300))
+game_name = font.render('Pixel Runner', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center = (400, 80))
 
-player_surf= pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-player_rect = player_surf.get_rect(midbottom = (80, 300))
-player_gravity = 0
+game_message = font.render('Press space to run', False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center = (400, 340))
+
+# Timer
+#  You have to add plus 1 to not conflict with pygame's own userevents
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
+
+snail_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(snail_animation_timer, 500)
+
+fly_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(fly_animation_timer, 200)
 
 
 while True:
+    # Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         if game_active:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
-                    player_gravity = -20
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                    print('space')
-                    player_gravity = -20
+            # Timers
+            if event.type == obstacle_timer:
+                # choice chooses a random item out of the lsit so this will create a fly 25 procent of the time and snail 75 % of the time
+                obstacle_group.add(Obstacle(choice(['fly', 'snail', 'snail', 'snail'])))
         else:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and event.type == pygame.KEYDOWN:
                     game_active = True
-                    snail_rect.left = 800
+                    start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
-
         # If you dont setup a background you will be able to see previous frames so make sure to set a background
         screen.blit(sky, (0, 0))
         screen.blit(ground, (0, 300))
-        pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-        screen.blit(score_surf, score_rect)
 
-        # Move the snail to the right when it leaves the screen on the left
-        snail_rect.x -= 4
-        if snail_rect.right <-100: snail_rect.left = 800
-        screen.blit(snail_surf, snail_rect)
+        # Score
+        score = display_score()
 
-        # player
-        # gravity
-        player_gravity += 1
-        player_rect.y += player_gravity
-        # make him not fall through the ground
-        if player_rect.bottom >= 300 : player_rect.bottom = 300
-        screen.blit(player_surf, player_rect)
+        # Player
+        player.draw(screen)
+        player.update()
 
-        # End game if user touches the snail
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+        # Obstacle
+        obstacle_group.draw(screen)
+        obstacle_group.update()
+
+        # Collision
+        game_active = collision_sprite()
 
     else:
-        screen.fill('Yellow')
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rect)
+
+        score_message = font.render(f'Your score: {score}', False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center = (400, 330))
+        screen.blit(game_name, game_name_rect)
+
+        if score == 0: screen.blit(game_message, game_message_rect)
+        else: screen.blit(score_message, score_message_rect)
     
     # Update everything
     pygame.display.update() 
